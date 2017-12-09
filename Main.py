7aa713aea7d6254c2eb295bb353fade390d5d59c
Ekats <ekkekaha@gmail.com>
2017-12-09 13:20:54 +0200

    more files, open/close window

--- Main.py
+++ Main.py
@@ -1,3 +1,24 @@
 import pygame
 
 pygame.init()
+
+windowX = 800
+windowY = 600
+gameDisplay = pygame.display.set_mode((windowX,windowY))
+clock = pygame.time.Clock()
+gameExit = False
+
+pygame.display.set_caption("Ööjooksu simulaator")
+
+while not gameExit:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			gameExit = True
+	if event.type == pygame.KEYDOWN:
+		if event.key == pygame.K_ESCAPE:
+			gameExit = True
+	gameDisplay.fill((0, 0, 0))
+	pygame.display.update()
+
+pygame.quit()
+quit()
